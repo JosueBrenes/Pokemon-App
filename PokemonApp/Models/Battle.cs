9db848c9fca363023a38785849f5e0a5fe0d0881
JosAry25 <josaraya6183@gmail.com>
2024-11-10 16:26:26 -0600

    feat: Models for Beattle and Challenges
    Models for Beattle and Challenges

---
+++ PokemonApp/Models/Battle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PokemonApp.Models
+{
+    public class Battle
+    {
+        public int Id { get; set; }
+        public int ChallengeId { get; set; }
+        public Challenge Challenge { get; set; }
+        public string OpponentName { get; set; }
+        public int RewardPoints { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+
+}
